bd70ba9d863a2eabd4e8d0c9b5929425109c4a38
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-27 15:28:35 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,21 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loops.`)
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while(n > 0) {
+    console.log(`${n}`)
+    n--
+  }
+  return "done"
+}
+
+function doWhileLoop(array) {
+  while(array.length > 0) {
+    array.pop()
+  }
+  return array
+}
